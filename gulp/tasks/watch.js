97264bd0dd724f259df52dcacc731d152ca6f4e0
Kriti Kothari <kritikothari2000@gmail.com>
2020-07-17 02:03:25 +0530

    Completed desktop and mobile header styles

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -15,6 +15,7 @@ gulp.watch('./app/index.html', function(){
   browserSync.reload()
 });
 gulp.watch('./app/assets/styles/**/*.css', gulp.series('cssInject'));
+gulp.watch('./app/assets/scripts/**/*.js', gulp.series('scripts'));
 });
 
 gulp.task('cssInject', gulp.series('styles', function(){
